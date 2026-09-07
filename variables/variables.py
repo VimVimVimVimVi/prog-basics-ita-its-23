5f0912a719c06da9e300567a78d3e9d2eb5f7159
VimVimVimVimVi <aleks.toomeoja@voco.ee>
2026-09-07 12:50:00 +0000

    HELL IT'S HELL HERE

---
+++ variables/variables.py
@@ -0,0 +1,31 @@
+# S
+
+def kill(Y, A):
+    sell = Y + " " + A
+    print(sell)
+
+word = "spider"
+# p
+thousand = "1000"
+# i
+ten = 10
+# d
+first_half = "full" + str(ten)
+# e
+second_half = "moon"
+# r
+complete = first_half + " " + second_half
+
+# s
+print((first_half + " " + second_half))
+print(thousand)
+kill(first_half, second_half)
+# "Red, yellow, red, yell-"
+# "Shut up!"
+
+DOOM = input("")
+
+if DOOM == "Red, yellow, red, yellow":
+    print("Shut up!")
+else:
+    print(DOOM)
